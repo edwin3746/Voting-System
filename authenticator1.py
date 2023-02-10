7957ea82eebddb98db8816f9df410064344f52c0
YrReJ <91773813+Jasawn@users.noreply.github.com>
2023-02-10 17:52:27 +0800

    Update authenticator1.py

--- authenticator1.py
+++ authenticator1.py
@@ -2,33 +2,45 @@ import socket
 from Cryptodome.Util import number
 from Cryptodome.Random import get_random_bytes
 
+## Pip install pycryptodomex
+
 server_address = ('127.0.0.1',7777)
-auth1_address = ('127.0.0.2',1234)
+port = number.getRandomRange(1, 65536)
+auth1_address = ('127.0.0.2',port)
 
 def retrievePublicKeys(receivePubKeyInfo):
     receivePubKeyInfo.connect(server_address)
+    pubKeyInfo = ""
+    p = ""
     q = ""
+    g = ""
 
-    while not q:
-        receivePubKeyInfo.sendall(b'Retrieve public key parameters')
-        q = receivePubKeyInfo.recv(8192).decode("utf-8")
+    while not pubKeyInfo or not p or not q or not g:
+        receivePubKeyInfo.send(b'Retrieve public key parameters')
+        pubKeyInfo = receivePubKeyInfo.recv(8192*10).decode("utf-8")
+        p = pubKeyInfo.split("||")[0]
+        q = pubKeyInfo.split("||")[1]
+        g = pubKeyInfo.split("||")[2]
+    if p and q and g:
+        receivePubKeyInfo.send(b"Received q")
 
-    receivePubKeyInfo.sendall(b"Received q")
     receivePubKeyInfo.close()
 
     ## Generate part of private key here (g^x mod p)
-    partialPrivateKey = number.getRandomRange(2,q-2)
+    partialPrivateKey = number.getRandomRange(2,int(q)-2)
 
     ## Commitment
 def main():
     auth1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    auth1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     auth1.bind(auth1_address)
-    retrievePublicKeys(auth1)
-    #try:
-
-    #except:
-    #    print("Server is not up!")
+    try:
+        retrievePublicKeys(auth1)
+    except:
+        print("Server is not up!")
+    auth1.close()
 
 if __name__ == "__main__":
     main()
+
 
