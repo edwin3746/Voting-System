f4516d9f2280fab324fde3e61dc11316c5575174
YrReJ <91773813+Jasawn@users.noreply.github.com>
2023-02-10 17:09:10 +0800

    Update authenticator1.py

--- authenticator1.py
+++ authenticator1.py
@@ -1,4 +1,6 @@
 import socket
+from Cryptodome.Util import number
+from Cryptodome.Random import get_random_bytes
 
 server_address = ('127.0.0.1',7777)
 auth1_address = ('127.0.0.2',1234)
@@ -9,19 +11,23 @@ def retrievePublicKeys(receivePubKeyInfo):
 
     while not q:
         receivePubKeyInfo.sendall(b'Retrieve public key parameters')
-        q = receivePubKeyInfo.recv(8192*10).decode("utf-8")
+        q = receivePubKeyInfo.recv(8192).decode("utf-8")
+
+    receivePubKeyInfo.sendall(b"Received q")
+    receivePubKeyInfo.close()
 
     ## Generate part of private key here (g^x mod p)
     partialPrivateKey = number.getRandomRange(2,q-2)
+
     ## Commitment
 def main():
     auth1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     auth1.bind(auth1_address)
+    retrievePublicKeys(auth1)
+    #try:
 
-    try:
-        retrievePublicKeys(auth1)
-    except:
-        print("Server is not up!")
+    #except:
+    #    print("Server is not up!")
 
 if __name__ == "__main__":
     main()
