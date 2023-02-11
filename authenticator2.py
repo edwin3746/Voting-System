6ca6c18bf66301ca2ded3fe42f3065d273f29441
YrReJ <91773813+Jasawn@users.noreply.github.com>
2023-02-12 00:12:01 +0800

    Update authenticator2.py

--- authenticator2.py
+++ authenticator2.py
@@ -14,11 +14,17 @@ def generate_r(q):
     return r
 
 def sendCommitment(commitmentInfo,server):
+    count = 0
     server.connect(server_address)
-    if server.recv(1024).decode("utf-8") == "Connection is secure":
-        server.sendall(commitmentInfo)
-    if server.recv(1024).decode("utf-8") == "Valid":
-        server.close()
+    while True:
+        if server.recv(1024).decode("utf-8") == "Connection is secure":
+            server.sendall(commitmentInfo)
+        if server.recv(1024).decode("utf-8") == "Valid":
+            server.close()
+            break
+        count += 1
+        if count == 10:
+            raise Exception()
 
 def retrievePublicKeys(receivePubKeyInfo):
     count = 0
@@ -41,12 +47,11 @@ def retrievePublicKeys(receivePubKeyInfo):
     if p and q and g:
         receivePubKeyInfo.sendall(b"Received Q!")
 
-    msgCode = receivePubKeyInfo.recv(1024).decode("utf-8")
-    while msgCode != "Partial Private Key Generated Complete!":
-        print(wait)
-        wait += "."
-    print("Done")
-    receivePubKeyInfo.close()
+    while True:
+        if receivePubKeyInfo.recv(1024).decode("utf-8") == "Partial Private Key Generated Complete!":
+            time.sleep(7)
+            receivePubKeyInfo.close()
+            break
 
     ## Generate part of private key here (g^x mod p)
     partialx = number.getRandomRange(2,int(q)-2)
@@ -57,26 +62,40 @@ def retrievePublicKeys(receivePubKeyInfo):
     secret = (pow(g,partialPrivateKey,p) * pow(partialPrivateKey, r, p)) % p
     return secret,partialPrivateKey,r
 
-def main():
+def startSocket():
     auth2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     auth2.bind(auth2_address)
+    return auth2
+
+def main():
+    auth2 = startSocket()
     secret = ""
     partialPrivateKey = ""
     r = ""
+    count = 0
 
-    secret,partialPrivateKey,r = retrievePublicKeys(auth2)
+    while not secret or not partialPrivateKey or not r:
+        try:
+            secret,partialPrivateKey,r = retrievePublicKeys(auth2)
+        except:
+            print("An error has occured")
+            count += 1
+        if count == 10:
+            print("Please restart the server")
 
-    print(secret)
-    #commitmentInfo = str(secret) + "||" + str(partialPrivateKey) + "||" + str(r)
+
+    print(partialPrivateKey)
+    commitmentInfo = str(secret) + "||" + str(partialPrivateKey) + "||" + str(r)
     ## Convert the commitmentInfo into bytes and send to server
-    #commitmentInfo = str.encode(str(commitmentInfo))
-    #sendCommitment(commitmentInfo,auth1)
+    commitmentInfo = str.encode(str(commitmentInfo))
+
+    auth2 = startSocket()
+    sendCommitment(commitmentInfo,auth2)
 
     auth2.close()
 
 if __name__ == "__main__":
     main()
-
 
 
 
