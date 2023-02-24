0fb9b3ef574ce13b555597b119e727cba7f84586
YrReJ <91773813+Jasawn@users.noreply.github.com>
2023-02-24 18:28:24 +0800

    Socket is now running on TLS

--- authenticator2.py
+++ authenticator2.py
@@ -10,20 +10,39 @@ from server import server_address as server_address
 
 ## Pip install pycryptodomex
 
-port = number.getRandomRange(1, 65536)
+
 currentPath = os.getcwd()
-auth2_address = ('127.0.0.3',port)
+
 
 def generate_r(q):
     r = number.getRandomRange(2, q-2)
     return r
 
-def sendCommitment(commitmentInfo,server):
+def sendCommitment(commitmentValue,server):
     count = 0
     server.connect(server_address)
+
+    print("Sending Commitment to Server!")
+    ## Submit commitmentValue first
     while True:
         if server.recv(1024).decode("utf-8") == "Connection is secure":
-            server.sendall(commitmentInfo)
+            server.sendall(commitmentValue)
+        if server.recv(1024).decode("utf-8") == "Commitment Received":
+            server.close()
+            break
+        count += 1
+        if count == 10:
+            raise Exception()
+
+def sendEncryptedPartialPublicKey(partialPublicKeyInfo,server):
+    count = 0
+    server.connect(server_address)
+
+    print("Sending Encrypted Partial Public Key to Server!")
+    while True:
+        if server.recv(1024).decode("utf-8") == "Connection is secure":
+            print("Waiting")
+            server.sendall(partialPublicKeyInfo)
         if server.recv(1024).decode("utf-8") == "Valid":
             server.close()
             break
@@ -43,6 +62,7 @@ def retrievePublicKeys(receivePubKeyInfo):
     while not pubKeyInfo or not p or not q or not g:
         receivePubKeyInfo.sendall(b'Retrieve public key parameters')
         pubKeyInfo = receivePubKeyInfo.recv(8192*10).decode("utf-8")
+        print("Recevived Public Key Parameters!")
         p = int(pubKeyInfo.split("||")[0])
         q = int(pubKeyInfo.split("||")[1])
         g = int(pubKeyInfo.split("||")[2])
@@ -54,18 +74,21 @@ def retrievePublicKeys(receivePubKeyInfo):
 
     while True:
         if receivePubKeyInfo.recv(1024).decode("utf-8") == "Partial Private Key Generated Complete!":
-            time.sleep(7)
+            time.sleep(10)
             receivePubKeyInfo.close()
             break
 
-    ## Generate part of private key here (g^x mod p)
+    ## Generate part of public key here (g^x mod p)
     partialx = number.getRandomRange(2,int(q)-2)
+    print("Private Key Generated!")
     partialPublicKey = pow(g, partialx, p)
+    print("Partial Public Key Generated!")
 
     ## Commitment
     r = generate_r(q)
     secret = (pow(g,partialPublicKey,p) * pow(partialPublicKey, r, p)) % p
-    return secret,partialPublicKey,r
+    print("Commitment for Partial Public Key Generated!")
+    return partialx,secret,partialPublicKey,r
 
 # partial decrypt function
 def partialDecrypt(a, privateKey, p):
@@ -87,55 +110,58 @@ def hashThis(r, message):
     return int(hash.hexdigest(),16)
 
 def startSocket():
-    auth2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    auth2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    port = number.getRandomRange(1, 65536)
+    auth2_address = ('127.0.0.3',port)
+    auth2Connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    auth2Connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    auth2Connection.bind(auth2_address)
 
-    auth2 = ssl.wrap_socket(auth2, keyfile=currentPath+"\key.pem", certfile=currentPath+"\certificate.pem")
+    auth2TLS = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    auth2TLS.load_verify_locations(cafile="server.crt")
 
-    auth2.bind(auth2_address)
+    auth2 = auth2TLS.wrap_socket(auth2Connection, server_hostname="greek")
+
     return auth2
 
 def main():
+
     auth2 = startSocket()
     secret = ""
     partialPublicKey = ""
     r = ""
+    privateKey = ""
     count = 0
 
     # this message is to determine which server/authenticator sent the Schnorr signature
     message = "auth2"
     messageInASCII = ''.join(str(ord(c)) for c in message)
 
-    while not secret or not partialPublicKey or not r:
+    while not secret or not partialPublicKey or not r or not privateKey:
         try:
-            secret,partialPublicKey,r = retrievePublicKeys(auth2)
+            privateKey,secret,partialPublicKey,r = retrievePublicKeys(auth2)
         except:
             print("An error has occured")
             count += 1
         if count == 10:
             print("Please restart the server")
+            exit()
 
-
-    print(partialPublicKey)
-    commitmentInfo = str(secret) + "||" + str(partialPublicKey) + "||" + str(r)
+    partialPublicKeyInfo = str(partialPublicKey) + "||" + str(r)
     ## Convert the commitmentInfo into bytes and send to server
-    commitmentInfo = str.encode(str(commitmentInfo))
+    partialPublicKeyInfo = str.encode(partialPublicKeyInfo)
+    secret = str.encode(str(secret))
 
     auth2 = startSocket()
-    sendCommitment(commitmentInfo,auth2)
+    sendCommitment(secret,auth2)
+
+    time.sleep(15)
+    auth2 = startSocket()
+    sendEncryptedPartialPublicKey(partialPublicKeyInfo,auth2)
 
     auth2.close()
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
 
 
 
