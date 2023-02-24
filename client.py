150a8819749df4e792022267e6f5c5ad9e744e37
YrReJ <91773813+Jasawn@users.noreply.github.com>
2023-02-24 23:59:32 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,32 +1,51 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify, flash
 from Cryptodome.Util import number
 from Cryptodome.Random import get_random_bytes
+from server import server_address as server_address
+from server import receiveVote_address as receiveVote_address
 import hashlib
 import socket
 import time
+import os
+import ssl
+
 
 ## Pip install tinyec
 
-server_address = ('127.0.0.1', 7777)
 votePage = Flask(__name__)
+currentPath = os.getcwd()
 candidates = []
 votingEnd = ""
 pParamBytes = ""
 gParamBytes = ""
+qParamBytes = ""
 vote_str = ""
 publicKey = ""
 
-def startServer():
+def startSocket(i):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.connect(server_address)
+
+    serverTLS = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    serverTLS.load_verify_locations(cafile="server.crt")
+
+    server = serverTLS.wrap_socket(server, server_hostname="greek")
+
+    if i == 1:
+        server.connect(server_address)
+    elif i == 2:
+        server.connect(receiveVote_address)
     return server
 
+def generate_r(q):
+    r = number.getRandomRange(2, q-2)
+    return r
 
 def retrieveServerInformation(receiveInfo):
     global publicKey
     global votingEnd
     global pParamBytes
     global gParamBytes
+    global qParamBytes
     candidateNames = ""
     count = 0
 
@@ -73,19 +92,23 @@ def retrieveServerInformation(receiveInfo):
         if count == 10:
             raise Exception()
 
+    while not qParamBytes:
+        receiveInfo.send(b'Requesting Public Q')
+        qParamBytes = receiveInfo.recv(2048).decode("utf-8")
+        print(qParamBytes)
+        count += 1
+        if count == 10:
+            raise Exception()
+
     global candidates
     candidates.extend(candidateNames.split("||"))
     candidates[:] = [x for x in candidates if x != ""]
 
-    print(candidates)
-    print(votingEnd)
     receiveInfo.close()
-    return pParamBytes, gParamBytes, publicKey
+    return pParamBytes, gParamBytes, qParamBytes, publicKey
 
-def sendVotes(server, encryptedVotes):
-    server.send(encryptedVotes)
+def sendVotes(encryptedmessages):
     count = 0
-
     while True:
         msgCode = connection.recv(1024).decode("utf-8")
         if msgCode == "Vote has been tampered":
@@ -101,6 +124,32 @@ def sendVotes(server, encryptedVotes):
         count += 1
         if count == 10:
             raise Exception()
+    server.close()
+
+def sendCommitment(secret,encryptedmessages):
+    server = startSocket(2)
+    count = 0
+
+    while True:
+        if server.recv(1024).decode("utf-8") == "Connection is secure":
+            print("Secure")
+            server.sendall(secret)
+        if server.recv(1024).decode("utf-8") == "Commitment received!":
+            print("Sent")
+            server.sendall(encryptedmessages)
+            break
+        count += 1
+        if count == 10:
+            raise Exception()
+    while True:
+        msgCode = server.recv(1024).decode("utf-8")
+        if msgCode == "Vote is valid!":
+            print("Thank you for your vote!")
+            break
+        elif msgCode == "Vote is tampered!":
+            print("Invalid")
+            exit
+
 
 
 def encrypt(message, p, g, public_key):
@@ -109,11 +158,10 @@ def encrypt(message, p, g, public_key):
     b = (message * pow(public_key, k, p)) % p
     return a, b
 
-
 def main():
-    global pParamBytes, gParamBytes, publicKey
-    server = startServer()
-    pParamBytes, gParamBytes, publicKey = retrieveServerInformation(server)
+    global pParamBytes, gParamBytes, qParamBytes, publicKey
+    server = startSocket(1)
+    pParamBytes, gParamBytes, qParamBytes, publicKey = retrieveServerInformation(server)
     votePage.run()
 
 @votePage.route('/')
@@ -123,8 +171,9 @@ def vote_page():
 @votePage.route('/vote', methods=['POST'])
 def process_vote():
     global vote_list
+    encrypted_vote = ""
+    secret = ""
     vote = request.form['vote']
-
     #format candidate selection to numbers
     vote_str = '0' * len(candidates) # initializing the string with 0's
     candidate_index = int(vote) - 1 # getting the selected candidate
@@ -132,25 +181,20 @@ def process_vote():
 
     #convert each string in vote_list to int
     vote_list_bytes = [bytes(x, "utf-8") for x in vote_str]
-    print(vote_list_bytes)
 
     #call the encrypt function for each vote result in the vote list
-    encrypted_vote = []
     for vote in range(len(vote_list_bytes)):
-        print("Round ",vote)
-        print(vote_list_bytes[vote])
-        a, b = encrypt(int.from_bytes(vote_list_bytes[vote]), int(pParamBytes), int(gParamBytes), int(publicKey))
-        voteConcat = a + '|' + b
-        encrypted_vote.append(voteConcat + "||")
-    print(encrypted_vote)
+        a, b = encrypt(int.from_bytes(vote_list_bytes[vote], byteorder="big"), int(pParamBytes), int(gParamBytes), int(publicKey))
+        encrypted_vote = encrypted_vote + str(a) + "||" + str(b) + "***"
 
-    try:
-        sendVotes(encrypted_vote)
-    except:
-        print("An error has occured")
+        ## Generate Commitment for the server to validate that the votes are not tampered with
+        r = generate_r(int(qParamBytes))
+        secret = secret + str(pow(int(gParamBytes),(a+b),int(pParamBytes)) * pow((a+b), r, int(pParamBytes)) % int(pParamBytes)) + "||" + str(r) + "***"
 
+    encrypted_vote = str.encode(encrypted_vote)
+    secret = str.encode(secret)
+    sendCommitment(secret, encrypted_vote)
     return render_template("voteResult.html",  candidate_index=candidate_index, candidates=candidates, vote_str=vote_str)
-
 
 
 if __name__ == "__main__":
